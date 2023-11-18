271a402c3e5cedfaed9bbb4b598a312cb35f16cd
josemaria15 <150299185+josemaria15@users.noreply.github.com>
2023-11-18 15:52:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+drum_numbers = document.querySelectorAll(".drum").length
+
+for (i = 0 ; i<drum_numbers; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML)
+  buttonAnimation(buttonInnerHTML)
+  })
+
+
+
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key)
+  buttonAnimation(event.key)
+})
+
+
+function makeSound(key){
+  switch (key) {  
+    case "w":
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+    case "s":
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+    case "d":
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+    case "j":
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+    case "k":
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+    case "l":
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+    default:
+      break;
+    }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+
+    activeButton.classList.remove("pressed")
+  },100);
+}
